8959f4279710a4c65bc59c90202704bf2d5abd34
Persephone Dis <persephone-dis@yandex.ru>
2020-09-24 17:22:42 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 <head>
 	<?php
 	$website_title = 'Заявки';
-	require 'templates/head.php'; ?>
+	require 'templates/head.php';
+
+
+	?>
 </head>
 <body>
 <?php require 'templates/header.php' ?>
@@ -11,8 +14,18 @@
 	<div class="row">
 		<div class="col-md-8 mb-3">
 			<?php
-				require_once'mysql_connect.php';
-				$sql = "SELECT * FROM requests ORDER BY date DESC";
+			require_once'mysql_connect.php';
+			$sql = "SELECT * FROM requests ORDER BY date DESC";
+
+		if($_COOKIE['log']!='') {
+			echo "<a class='btn btn-info mt-5' href='?filter=Date'>По дате</a>";
+			echo "<a class='btn btn-info mt-5' href='?filter=NoAddress'>Нет адреса</a>";
+		}
+		if ($_GET["filter"]=="Date") {
+		$sql = "SELECT * FROM requests ORDER BY date";}
+		if ($_GET["filter"]=="NoAddress") {
+		$sql = "SELECT * FROM requests WHERE length(address)=0";}
+
 				$query = $pdo->query($sql);
 				while ($row = $query->fetch(PDO::FETCH_OBJ)) {
 					echo "<h2>$row->name</h2>
